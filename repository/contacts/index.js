d1389c54bbaef5031a5ae4719f11e20e3a698dbd
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-10-28 11:49:48 +0200

    Update index.js

--- repository/contacts/index.js
+++ repository/contacts/index.js
@@ -46,7 +46,7 @@ const getContactById = async (contactId, userId) => {
     path: 'owner',
     select: 'email subscription createdAt updatedAt',
   });
-  console.log(result);
+
   return result;
 };
 
